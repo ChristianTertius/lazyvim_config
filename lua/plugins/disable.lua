21b5f8a73e415c80006f4a0a4df7eb90dec573d3
ChristianTertius <ct160103@gmail.com>
2026-06-12 18:59:01 +0800

    feat: show hidden filex on snacks explorer

--- lua/plugins/disable.lua
+++ lua/plugins/disable.lua
@@ -7,6 +7,8 @@ return {
       picker = {
         sources = {
           explorer = {
+            hidden = true,
+            ignored = true,
             auto_close = true,
             layout = {
               layout = { position = "float", width = 0.5, height = 0.8 },
