7347ae62cf8dae90828364ab21b09ce82f431a86
ChristianTertius <ct160103@gmail.com>
2026-06-16 00:59:51 +0800

    chore: delete ubuntu lazyvim config

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -7,18 +7,23 @@
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
 
--- NOTE: ubuntu only di omarchy nd usa
-vim.api.nvim_create_autocmd("FileType", {
-  pattern = { "go", "javascript", "typescript", "typescriptreact", "json", "lua", "bash", "sh", "php", "blade" },
-  callback = function()
-    pcall(vim.treesitter.start)
-  end,
-})
-
 -- change time yank highlight
 vim.api.nvim_create_autocmd("TextYankPost", {
   group = vim.api.nvim_create_augroup("highlight_yank", { clear = true }),
   callback = function()
     vim.highlight.on_yank({ higroup = "IncSearch", timeout = 30 })
+  end,
+})
+
+vim.api.nvim_create_autocmd({ "BufReadPost", "BufWritePre" }, {
+  pattern = "*",
+  callback = function()
+    if vim.bo.binary or vim.bo.filetype == "" then
+      return
+    end
+    if not vim.bo.modifiable then
+      return
+    end
+    vim.cmd("%s/\r//ge")
   end,
 })
