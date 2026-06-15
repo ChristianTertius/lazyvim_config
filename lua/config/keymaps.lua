0469636b4a6ae26ec613d963ebfbf50bc1bc908f
ChristianTertius <ct160103@gmail.com>
2026-06-15 19:57:13 +0800

    feat: add tab keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -10,3 +10,9 @@ keymap.set("n", "<leader>p", "V$%", { desc = "block function" })
 keymap.set("n", "<leader>as", "o<esc>k", { noremap = true, silent = true, desc = "newline for normal mode" })
 keymap.set("n", "<leader>re", ":restart<CR>", { desc = "restart" })
 keymap.set("n", "<c-a>", "ggVG", { desc = "select all" })
+
+keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" }) -- open new tab
+keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" }) -- close current tab
+keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" }) -- go to text tab
+keymap.set("n", "<leader>tb", "<cmd>tabp<CR>", { desc = "Go to prev tab" }) -- go to previous tab
+keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" })
