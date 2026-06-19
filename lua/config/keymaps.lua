2f21635e8795fadf9e50ffa5b37c177f46b9b364
ChristianTertius <ct160103@gmail.com>
2026-06-19 13:09:52 +0800

    chore: chore disable record

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -5,10 +5,12 @@
 vim.g.mapleader = " "
 local keymap = vim.keymap
 
-keymap.set("i", "jk", "<esc>", { desc = "back to normal mode" })
+keymap.set("n", "q", "<nop>", opts)
+-- keymap.set("i", "jk", "<esc>", { desc = "back to normal mode" })
+-- keymap.set("i", "<esc>", "<esc>:w<CR>", { desc = "back to normla mode + save", silent = true })
 keymap.set("n", "<leader>p", "V$%", { desc = "block function" })
 keymap.set("n", "<leader>as", "o<esc>k", { noremap = true, silent = true, desc = "newline for normal mode" })
-keymap.set("n", "<leader>re", ":restart<CR>", { desc = "restart" })
+keymap.set("n", "<leader>re", ":restart<CR>", { desc = "restart", silent = true })
 keymap.set("n", "<c-a>", "ggVG", { desc = "select all" })
 
 keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" }) -- open new tab
